8f785e76a89bc18d2a16debc9c0c99ab1de4f4bc
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-14 15:08:53 +0100

    finish

--- 1_9/main.js
+++ 1_9/main.js
@@ -1,8 +1,12 @@
 const farbeAuswahlen = document.querySelector("#farbeAuswahlen")
 const button = document.querySelector("#button")
 
+farbeAuswahlen.addEventListener('change', () => {
+    console.log(farbeAuswahlen.selectedIndex)
+})
+
+
 button.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log(farbeAuswahlen.selectedIndex)
     farbeAuswahlen.remove(farbeAuswahlen.selectedIndex)
 })
