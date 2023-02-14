eb99195c6530529c7ba22c162ffff5dd70b453be
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-14 15:05:57 +0100

    finish

--- 1_9/main.js
+++ 1_9/main.js
@@ -1,26 +1,8 @@
 const farbeAuswahlen = document.querySelector("#farbeAuswahlen")
 const button = document.querySelector("#button")
 
-let selectedColor = 0
-let selectedChildElement
-
-
-
-farbeAuswahlen.addEventListener('change', (e) => {
-    console.log(e.target.selectedIndex)
-    selectedColor = e.target.selectedIndex
-    selectedChildElement = farbeAuswahlen.children[selectedColor]
-    console.log(selectedChildElement)
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(farbeAuswahlen.selectedIndex)
+    farbeAuswahlen.remove(farbeAuswahlen.selectedIndex)
 })
-
-
-button.addEventListener('click', () => {
-    
-
-    selectedChildElement.remove()
-    farbeAuswahlen.children[selectedColor].remove()
-    /* farbeAuswahlen.remove() */
-    /* button.remove() */
-})
-
-
